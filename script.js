2a117140363644ef6ae0115575c14455beafdd91
ZedZagg <zedzagg@gmail.com>
2023-12-06 14:23:32 +1100

    Include files that should've been in the last commit

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
 console.log("script has loaded!");
+
+let weaponEl;
+let weaponOffset = {"x": 0, "y": 0};
+function initWeapon(){
+  weaponEl = document.getElementById("weapon");
+  
+  // buster sword offsets
+  weapon.style.transform = "scaleX(-1) rotate(90deg)";
+  weapon.style.transformOrigin = "bottom right";
+  weaponOffset.x = -360;
+  weaponOffset.y = 40;
+}
+function showWeapon(){
+  weaponEl.style.visibility = "visible";
+}
+function hideWeapon(){
+  weaponEl.style.visibility = "hidden";
+}
+function moveWeapon(event){
+  weaponEl.style.top = event.clientY + weaponOffset.y + "px";
+  weaponEl.style.left = event.clientX + + weaponOffset.x + "px";
+}
