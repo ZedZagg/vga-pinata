b2add4f2eaf8a1b3fbe19dce6b13e0880958b3b3
ZedZagg <zedzagg@gmail.com>
2023-12-06 15:31:42 +1100

    Add weapon swing animation

--- script.js
+++ script.js
@@ -6,10 +6,9 @@ function initWeapon(){
   weaponEl = document.getElementById("weapon");
   
   // buster sword offsets
-  weapon.style.transform = "scaleX(-1) rotate(90deg)";
-  weapon.style.transformOrigin = "bottom right";
-  weaponOffset.x = -360;
-  weaponOffset.y = 40;
+  weapon.style.transformOrigin = "bottom left";
+  weaponOffset.x = -280;
+  weaponOffset.y = -80;
 }
 function showWeapon(){
   weaponEl.style.visibility = "visible";
@@ -21,3 +20,14 @@ function moveWeapon(event){
   weaponEl.style.top = event.clientY + weaponOffset.y + "px";
   weaponEl.style.left = event.clientX + + weaponOffset.x + "px";
 }
+function swingWeapon(){
+  const weaponSwingAnim = [
+    { transform: "rotate(0)" },
+    { transform: "rotate(35deg)", offset: 0.2 },
+    { transform: "rotate(35deg)", offset: 0.5 },
+    { transform: "rotate(0)" },
+  ]
+  const weaponSwingTiming = { duration: 500, iterations: 1 }
+  weaponEl.animate(weaponSwingAnim, weaponSwingTiming);
+}
+
